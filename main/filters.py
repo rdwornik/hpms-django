c5a87eb641abdd20c7828d77eeb6e17be9d88ee1
robertdwornik <robert.dwornik@outlook.com>
2020-04-26 16:29:30 +0200

    time filter works

--- main/filters.py
+++ main/filters.py
@@ -21,9 +21,6 @@ VISITOR_IP_CHOICES = [(value, id) for id, value in get_visitor_ip_choice_list()]
 from django.contrib.postgres.forms  import RangeWidget, DateTimeRangeField
 
 class TransactionsFilter(django_filters.FilterSet):
-    # time = django_filters.NumberFilter(method="time_filter",
-    #                                    widget=NumberInput(attrs={"placeholder": "Hours from now"}))
-    
     ip = django_filters.ChoiceFilter(method="ip_filter",
                                      choices=VISITOR_IP_CHOICES,
                                      widget=autocomplete.ListSelect2(url="visitors-ip-list-autocomplete",
@@ -41,9 +38,6 @@ class TransactionsFilter(django_filters.FilterSet):
     class Meta:
         model = models.Transaction
         fields = ["time","server","ip","tags"]    
-    def time_filter(self, queryset, name, value):
-        time_threshold = datetime.now() - timedelta(hours=int(value))
-        return queryset.filter(time__gt=time_threshold)
     def ip_filter(self, queryset, name, value):
         return queryset.filter(Q(name__header_name=settings.VISITORS_IP) & Q(value=value))
     def server_filter(self, queryset, name, value):
