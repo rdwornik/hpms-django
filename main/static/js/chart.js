d156073b1c496d8aaf081805b732269ed5ef249b
robertdwornik <robert.dwornik@outlook.com>
2020-05-06 11:37:41 +0200

    nice creating query parameters look nice in chart.js vol2

--- main/static/js/chart.js
+++ main/static/js/chart.js
@@ -1,13 +1,11 @@
-var canvas = document.getElementById('myChart')
+var canvas = document.getElementById("myChart")
 URL_ENDPOINT = canvas.attributes.getNamedItem("url-endpoint-chart").value;
 QUERY_PARAMS = window.location.search;
 URL = "".concat(URL_ENDPOINT,QUERY_PARAMS)
-console.log(QUERY_PARAMS)
 $.ajax({
     method:"GET",
     url: URL,
     success: function(data){
-        console.log(data)
         setChart(data)
     },     
     error: function(error_data){
@@ -17,15 +15,15 @@ $.ajax({
 })
 function setChart(data)
 {
-    var ctx = canvas.getContext('2d')
+    var ctx = canvas.getContext("2d")
     var myChart = new Chart(ctx, 
     {
-        type: 'line',
+        type: "line",
         data: 
           {
             datasets: 
               [{
-                  label: 'Number of transactions',
+                  label: "Number of transactions",
                   data: data.data,
               }]
           },
@@ -35,8 +33,8 @@ function setChart(data)
             {
               xAxes: 
                 [{
-                    type: 'time',
-                    distribution: 'series',
+                    type: "time",
+                    distribution: "series",
                     time: 
                         {
                             unit: data.label,
@@ -75,16 +73,16 @@ function setChart(data)
       var activePoints = myChart.getElementsAtEvent(evt);
       if (activePoints[0]) 
       {
-          chartData = activePoints[0]['_chart'].config.data
-          idx = activePoints[0]['_index']
+          chartData = activePoints[0]["_chart"].config.data
+          idx = activePoints[0]["_index"]
           value = chartData.datasets[0].data[idx]
           tag_field = this.attributes.getNamedItem("tag-field").value
           query = new URLSearchParams(window.location.search)
           assigned_tags = query.has(tag_field) ? query.get(tag_field) : ""
           params = {
             [tag_field] : assigned_tags,
-            "time_0" : value['x'],
-            "time_1" : value['x'].substring(0,10).concat(" 23:59"),
+            "time_0" : value["x"],
+            "time_1" : value["x"].substring(0,10).concat(" 23:59"),
           }
           search = new URLSearchParams(params)
           transactions = this.attributes.getNamedItem("transactions").value
