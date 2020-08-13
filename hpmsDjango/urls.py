0ab8c48302128c807c92da468efb9af08d968d62
robertdwornik <robert.dwornik@outlook.com>
2020-08-13 16:21:17 +0200

    select odwrócony page

--- hpmsDjango/urls.py
+++ hpmsDjango/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/hpms3/login'), name='login_redirect'),
+    path('hpms3/', RedirectView.as_view(url='/hpms3/login'), name='login_redirect'),
     path('hpms3/admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('hpms3/', include("main.urls")),
